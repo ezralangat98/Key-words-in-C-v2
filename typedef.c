fe10d83f75e5bfd168dd2f8f8fb481cd2dae80c8
Ezra <ezralangat88@gmail.com>
2021-10-04 10:42:16 -0700

    added more fieds on: 04102021_1041

--- typedef.c
+++ typedef.c
@@ -2,7 +2,7 @@
 #include <string.h>
 //Can be use to give a type a new name
 typedef unsigned char BYTE;
-BYTE  b1, b2;
+BYTE  b1[100], b2;
 //Typedef can also be used to gives a name to the user defined data types
 //Below it defines a new data type and then use that data type to define 
 //structure variables directly
@@ -22,11 +22,13 @@ int main( ) {
    strcpy( book.author, "Nuha Ali"); 
    strcpy( book.subject, "C Programming Tutorial");
    book.book_id = 6495407;
+   BYTE b1[100] = "This is so cool!";
  
    printf( "Book title : %s\n", book.title);
    printf( "Book author : %s\n", book.author);
    printf( "Book subject : %s\n", book.subject);
    printf( "Book book_id : %d\n", book.book_id);
+   printf("%s", b1);
 
    return 0;
 }
