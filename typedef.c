4f4c00155f57eafcf73f5c9dfaf8800db76a923d
Ezra <ezralangat88@gmail.com>
2021-10-04 10:36:56 -0700

    added more fieds on: 04102021

---
+++ typedef.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+//Can be use to give a type a new name
+typedef unsigned char BYTE;
+BYTE  b1, b2;
+//Typedef can also be used to gives a name to the user defined data types
+//Below it defines a new data type and then use that data type to define 
+//structure variables directly
+typedef struct Books {
+   char title[50];
+   char author[50];
+   char subject[100];
+   char editor[100];
+   int book_id;
+} Book;
+ 
+int main( ) {
+
+   Book book;
+ 
+   strcpy( book.title, "C Programming");
+   strcpy( book.author, "Nuha Ali"); 
+   strcpy( book.subject, "C Programming Tutorial");
+   book.book_id = 6495407;
+ 
+   printf( "Book title : %s\n", book.title);
+   printf( "Book author : %s\n", book.author);
+   printf( "Book subject : %s\n", book.subject);
+   printf( "Book book_id : %d\n", book.book_id);
+
+   return 0;
+}
